a3d0ce1809b99df62c168d65fd3ca8d4a7e0ab0e
Brandon Kudo and Emily Wells Jimenez <brandon+emily@>
2017-12-11 16:08:32 -0800

    add dynamic routing to album model.

--- src/app/album-detail/album-detail.component.spec.ts
+++ src/app/album-detail/album-detail.component.spec.ts
@@ -1,19 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MarketplaceComponent } from './marketplace.component';
 
-describe('MarketplaceComponent', () => {
-  let component: MarketplaceComponent;
-  let fixture: ComponentFixture<MarketplaceComponent>;
+import { AlbumDetailComponent } from './album-detail.component';
+
+describe('AlbumDetailComponent', () => {
+  let component: AlbumDetailComponent;
+  let fixture: ComponentFixture<AlbumDetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MarketplaceComponent ]
+      declarations: [ AlbumDetailComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MarketplaceComponent);
+    fixture = TestBed.createComponent(AlbumDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
